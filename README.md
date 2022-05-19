ab3f09502c7d6b2cf267a63f4032135f9f331822
willphp <24203741@qq.com>
2022-05-19 11:49:43 +0800

    v1.1.0

--- README.md
+++ README.md
@@ -1,45 +1,37 @@
 # 日志管理
+
 log组件用于记录网站日志
 
 #开始使用
 
 ####安装组件
-使用 composer 命令进行安装或下载源代码使用(依赖willphp/config组件)。
+
+使用 composer 命令进行安装或下载源代码使用。
 
     composer require willphp/log
 
-> WillPHP 框架已经内置此组件，无需再安装。
+> WillPHP框架已经内置此组件，无需再安装。
 
 ####调用示例
 
-    \willphp\log\Log::dir('./log')->write('错误信息', 'ERROR'); //写入错误信息
+    \willphp\log\Log::dir(__DIR__.'/log')->write('错误信息', 'ERROR'); 
 
 ####日志目录
 
-`config/log.php`配置文件可设置默认日志目录：
+定义默认目录：
 	
-	'dir' => './log', //默认日志目录
+	define('LOG_PATH', __DIR__.'/log');
 	
+自定义目录：
+	
+	Log::dir(__DIR__.'/log');
 
 ####记录日志
+
 记录日志会在请求结束时自动写入日志文件
 
-    Log::record('日志信息', Log::DEBUG);  //第二个参数为日志级别
+    Log::record('日志信息', 'DEBUG');  
 
 ####写入日志
 
-    Log::write('日志信息', Log::ERROR);  //第二个参数为日志级别
-
-
-####日志级别
-
-	const FATAL = 'FATAL';          //严重错误: 导致系统崩溃无法使用
-	const ERROR = 'ERROR';          //一般错误: 一般性错误
-	const WARNING = 'WARNING';      //警告性错误: 需要发出警告的错误
-	const NOTICE = 'NOTICE';        //通知: 程序可以运行但是还不够完美
-	const DEBUG = 'DEBUG';          //调试: 调试信息
-	const SQL = 'SQL';              //SQL：调试模式SQL语句
-	const EXCEPTION = 'EXCEPTION';  //异常错误	
-
-
-
+    Log::write('日志信息', 'ERROR');  
--- src/Log.php
+++ src/Log.php
@@ -3,31 +3,58 @@
  | Software: [WillPHP framework]
  | Site: www.113344.com
  |--------------------------------------------------------------------------
- | Author: no-mind <24203741@qq.com>
+ | Author: 无念 <24203741@qq.com>
  | WeChat: www113344
  | Copyright (c) 2020-2022, www.113344.com. All Rights Reserved.
  |-------------------------------------------------------------------------*/
 namespace willphp\log;
-use willphp\log\build\Base;
 class Log {
-	const FATAL = 'FATAL';          //严重错误: 导致系统崩溃无法使用
-	const ERROR = 'ERROR';          //一般错误: 一般性错误
-	const WARNING = 'WARNING';      //警告性错误: 需要发出警告的错误
-	const NOTICE = 'NOTICE';        //通知: 程序可以运行但是还不够完美
-	const DEBUG = 'DEBUG';          //调试: 调试信息
-	const SQL = 'SQL';              //SQL：调试模式SQL语句
-	const EXCEPTION = 'EXCEPTION';  //异常错误	
-	protected static $link;	
+	protected static $link;
 	public static function single()	{
 		if (!self::$link) {
-			self::$link = new Base();
+			self::$link = new LogBuilder();
 		}
 		return self::$link;
-	}	
+	}
 	public function __call($method, $params) {
 		return call_user_func_array([self::single(), $method], $params);
-	}	
+	}
 	public static function __callStatic($name, $arguments) {
 		return call_user_func_array([self::single(), $name], $arguments);
+	}
+}
+class LogBuilder {
+	protected $dir;
+	protected $log = [];
+	public function __construct() {
+		$dir = defined('LOG_PATH')? LOG_PATH : __DIR__.'/log';
+		$this->dir($dir);
+	}
+	public function dir($dir) {
+		if ($this->createDir($dir)) {
+			throw new \Exception('Log directory creation failed or is not writable.');
+		}
+		$this->dir = $dir;
+		return $this;
+	}
+	protected function createDir($dir, $auth = 0755) {
+		if (!empty($dir)) {
+			return is_dir($dir) or mkdir($dir, $auth, true);
+		}
+		return false;
+	}
+	public function record($message, $level = 'ERROR') {
+		$this->log[] = date('[ c ]').$level.':'.$message.PHP_EOL;
+		return true;
+	}
+	public function write($message, $level = 'ERROR') {
+		$file = $this->dir.'/'.date('Y_m_d').'.log';
+		return error_log(date('[ c ]').$level.':'.$message.PHP_EOL, 3, $file, null);
+	}
+	public function __destruct() {
+		if (!empty($this->log)) {
+			$file = $this->dir.'/'.date('Y_m_d').'.log';
+			return error_log(implode('', $this->log), 3, $file, null);
+		}
 	}
 }
